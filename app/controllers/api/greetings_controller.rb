5a71a76fc4e8acf8a6532c15d3eb3447ef46ec1e
RichardChileya <richardchileyawk@gmail.com>
2023-03-01 10:04:11 +0200

    Add controllers

---
+++ app/controllers/api/greetings_controller.rb
@@ -0,0 +1,8 @@
+class Api::GreetingsController < ApplicationController
+  def index
+    count_greetings = Greeting.count
+    random_offset = rand(count_greetings)
+    greetings = Greeting.offset(random_offset).first
+    render json: greetings
+  end
+end
